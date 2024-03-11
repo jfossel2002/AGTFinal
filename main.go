ab5537bd7c3347065ff81028aafb9bd3c9f55279
JFossel <jfossel@conncoll.edu>
2024-03-11 14:05:06 -0600

    Add JSON Reading - Added the ability to read from a json file

--- main.go
+++ main.go
@@ -3,17 +3,29 @@ package main
 
 import (
 	primary "AGT_Midterm/src"
+	voting_systems "AGT_Midterm/src/systems"
 )
 
 func main() {
+	canidatesFilePath := "canidates.json"
+	votersFilePath := "voters.json"
 
-	primary.RunSpecificInstance()
-	//runScenario(100000, numCandidates, maxPosition, minPosition, totalVoters)
-	/*
-		minPosition := 0.0
-		maxPosition := 1.0
-		numCandidates := 4
-		totalVoters := 50
-	*/
+	candidatesInterface, err := voting_systems.ReadFromFile(canidatesFilePath, "Candidate")
+	candidates := candidatesInterface.([]voting_systems.Candidate)
+
+	votersInterface, err := voting_systems.ReadFromFile(votersFilePath, "Voter")
+	voters := votersInterface.([]voting_systems.Voter)
+
+	if err != nil {
+		panic(err)
+	}
+
+	primary.RunSpecificInstance(candidates, voters)
+	/*minPosition := 0.0
+	maxPosition := 1.0
+	numCandidates := 4
+	totalVoters := 50
+
+	primary.RunScenario(1000000, numCandidates, maxPosition, minPosition, totalVoters)*/
 
 }
